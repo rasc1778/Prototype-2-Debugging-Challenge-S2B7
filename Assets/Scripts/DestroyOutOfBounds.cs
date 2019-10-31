b0763e61e8a04ac602bdbcda81e9dd6af5575c4e
Douglas Urner <dlu@canishe.com>
2019-10-30 22:13:03 -0700

    Create DestroyOutOfBounds script.

---
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+  float topBound = 30.0f;
+  float bottomBound = -30.0f;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (transform.position.z > topBound || transform.position.z < bottomBound)
+    {
+      Destroy(gameObject);
+    }
+
+  }
+}
