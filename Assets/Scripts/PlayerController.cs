cd53ef733746c614e8fc1cd1ac0755ae237ee209
Douglas Urner <dlu@canishe.com>
2019-10-30 21:15:43 -0700

    PlayerController script.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] string axisName = "Set Input Axis";
+    [SerializeField] float speed = 10.0f;
+    [SerializeField] float bound = 12.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    [SerializeField] float horizontalInput;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis(axisName);
+        transform.Translate(horizontalInput * speed * Time.deltaTime * Vector3.right);
+
+        if (transform.position.x < -bound) {
+            transform.position = new Vector3(-bound, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > bound) {
+            transform.position = new Vector3(bound, transform.position.y, transform.position.z);
+        }
+    }
+}
