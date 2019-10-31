bdfda8ae223188578d5c922a947bef6e50c9e6fb
Douglas Urner <dlu@canishe.com>
2019-10-30 21:28:20 -0700

    Clean up formatting.
    Use "Format Document" settings.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,29 +4,31 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] string axisName = "Set Input Axis";
-    [SerializeField] float speed = 10.0f;
-    [SerializeField] float xBound = 12.0f;
+  [SerializeField] string axisName = "Set Input Axis";
+  [SerializeField] float speed = 10.0f;
+  [SerializeField] float xBound = 12.0f;
 
-    // Start is called before the first frame update
-    void Start()
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  [SerializeField] float horizontalInput;
+
+  // Update is called once per frame
+  void Update()
+  {
+    horizontalInput = Input.GetAxis(axisName);
+    transform.Translate(horizontalInput * speed * Time.deltaTime * Vector3.right);
+
+    if (transform.position.x < -xBound)
     {
-        
+      transform.position = new Vector3(-xBound, transform.position.y, transform.position.z);
     }
-
-    [SerializeField] float horizontalInput;
-
-    // Update is called once per frame
-    void Update()
+    if (transform.position.x > xBound)
     {
-        horizontalInput = Input.GetAxis(axisName);
-        transform.Translate(horizontalInput * speed * Time.deltaTime * Vector3.right);
-
-        if (transform.position.x < -xBound) {
-            transform.position = new Vector3(-xBound, transform.position.y, transform.position.z);
-        }
-        if (transform.position.x > xBound) {
-            transform.position = new Vector3(xBound, transform.position.y, transform.position.z);
-        }
+      transform.position = new Vector3(xBound, transform.position.y, transform.position.z);
     }
+  }
 }
