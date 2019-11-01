60b65b4ca5392572889031c1b8412820c9cc057b
Douglas Urner <dlu@canishe.com>
2019-10-31 20:47:52 -0700

    Spawn animal when S key is pressed.

--- Assets/Scripts/SpanManager.cs
+++ Assets/Scripts/SpanManager.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class SpanManager : MonoBehaviour
 {
   public GameObject[] animalPrefabs;
-  
+
   // Start is called before the first frame update
   void Start()
   {
 
   }
 
+  [SerializeField] float spawnRangeX = 20;
+  [SerializeField] float spawnPosZ = 20;
+
   // Update is called once per frame
   void Update()
   {
-
+    if (Input.GetKeyDown(KeyCode.S)) {
+      Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+      int animalIndex = Random.Range(0, animalPrefabs.Length);
+      Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+    }
   }
 }
