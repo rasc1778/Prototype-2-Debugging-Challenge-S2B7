e19a23119d04bb5ff7a14708412aae2e270bed1b
Douglas Urner <dlu@canishe.com>
2020-02-06 06:54:44 -0800

    Reformat code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,39 +4,40 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-  [SerializeField] string axisName = "Set Input Axis";
-  [SerializeField] float speed = 10.0f;
-  [SerializeField] float xBound = 12.0f;
-  [SerializeField] GameObject projectilePrefab;
+    [SerializeField] string axisName = "Set Input Axis";
+    [SerializeField] float speed = 10.0f;
+    [SerializeField] float xBound = 12.0f;
+    [SerializeField] GameObject projectilePrefab;
 
-  // Start is called before the first frame update
-  void Start()
-  {
-
-  }
-
-  [SerializeField] float horizontalInput;
-
-  // Update is called once per frame
-  void Update()
-  {
-    // Fling food.
-    if (Input.GetKeyDown(KeyCode.Space)) {
-        // Launch projectile (food).
-        Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
-    }
-
-    // Player movement.
-    horizontalInput = Input.GetAxis(axisName);
-    transform.Translate(horizontalInput * speed * Time.deltaTime * Vector3.right);
-
-    if (transform.position.x < -xBound)
+    // Start is called before the first frame update
+    void Start()
     {
-      transform.position = new Vector3(-xBound, transform.position.y, transform.position.z);
+
     }
-    if (transform.position.x > xBound)
+
+    [SerializeField] float horizontalInput;
+
+    // Update is called once per frame
+    void Update()
     {
-      transform.position = new Vector3(xBound, transform.position.y, transform.position.z);
+        // Fling food.
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Launch projectile (food).
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+
+        // Player movement.
+        horizontalInput = Input.GetAxis(axisName);
+        transform.Translate(horizontalInput * speed * Time.deltaTime * Vector3.right);
+
+        if (transform.position.x < -xBound)
+        {
+            transform.position = new Vector3(-xBound, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xBound)
+        {
+            transform.position = new Vector3(xBound, transform.position.y, transform.position.z);
+        }
     }
-  }
 }
