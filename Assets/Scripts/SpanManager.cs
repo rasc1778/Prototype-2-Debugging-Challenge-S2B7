26aeb1573e3ab8719838ec83e9e08853f83d4440
Douglas Urner <dlu@canishe.com>
2020-02-06 06:57:32 -0800

    Code out of order

--- Assets/Scripts/SpanManager.cs
+++ Assets/Scripts/SpanManager.cs
@@ -4,29 +4,31 @@ using UnityEngine;
 
 public class SpanManager : MonoBehaviour
 {
-  public GameObject[] animalPrefabs;
+    public GameObject[] animalPrefabs;
 
-  [SerializeField] float spawnRangeX = 20;
-  [SerializeField] float spawnPosZ = 20;
-  [SerializeField] float spawnDelay = 2;
-  [SerializeField] float spawnInterval = 1.5f;
+    [SerializeField] float spawnRangeX = 20;
+    [SerializeField] float spawnPosZ = 20;
+    [SerializeField] float spawnDelay = 2;
+    [SerializeField] float spawnInterval = 1.5f;
 
-  // Start is called before the first frame update
-  void Start()
-  {
-    InvokeRepeating("SpawnRandomAnimal", spawnDelay, spawnInterval);
-  }
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal", spawnDelay, spawnInterval);
+    }
 
-  // Update is called once per frame
-  void Update()
-  {
+    // Update is called once per frame
+    void Update()
+    {
 
-  }
+    }
 
-  void SpawnRandomAnimal()
-  {
-    Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-    int animalIndex = Random.Range(0, animalPrefabs.Length);
-    Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
-  }
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = 0;
+
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+        animalIndex = Random.Range(0, animalPrefabs.Length);
+    }
 }
